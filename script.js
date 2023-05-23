c0f7752e54428974dfc99e9dbea891b005dcb847
LasaroAlmeida <lasarodalmeida@gmail.com>
2023-05-23 20:39:44 -0300

    configura por evento

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const form = document.forms.entrada;
-form.addEventListener("submit", envia);
+window.addEventListener('load', setup);
+
+function setup() {
+    const form = document.forms.entrada;
+    form.addEventListener("submit", envia);
+}
+
 function envia(event) {
     event.preventDefault();
     console.log("Formulário Enviado!");
