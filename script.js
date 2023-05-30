e70d01b3514d3476fafa31bc3b2e0c08fc6089f2
LasaroAlmeida <lasarodalmeida@gmail.com>
2023-05-30 19:27:35 -0300

    adiciona estado básico

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const form = document.forms.entrada;
 form.addEventListener("submit", envia);
+
+let estado = 0;
+
+
 function envia(event) {
     event.preventDefault();
     console.log("Formulário Enviado!");
+    estado++;
+    atualiza();
 }
+
+function atualiza() {
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
+}
+
+
